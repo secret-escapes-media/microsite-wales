d415d7b2a88cf43f46ffab5eee504d4fb75ed79f
Hamish Gray <hamishgray@Hamishs-MacBook-Pro.local>
2017-09-22 17:46:04 +0100

    Input content, design progress

--- js/_components/standard.js
+++ js/_components/standard.js
@@ -57,8 +57,61 @@ if ("ontouchstart" in document.documentElement){
   // add class to individual nav item
   $('.page--' + currentPage + ' [class*=nav__item--' + currentPage + ']').addClass('is-current');
 
-  // var currentCategory = $('body').data('current-category');
-  // // if there is a category, add class to category nav item
-  // if (currentCategory !== ''){
-  //   $('.category--' + currentCategory + ' [class*=nav__item--' + currentCategory + ']').addClass('is-current');
-  // }
+
+
+///////////////////////////////////////
+//       In page nav
+///////////////////////////////////////
+
+function stickNav(){
+  var st = $(document).scrollTop();
+  var trigger = $('.intro.has-nav');
+  var triggerH = $('.intro__content').outerHeight();
+  var distance = triggerH + trigger.offset().top;
+  var nav = $('.intro__nav');
+
+  if( st > distance ){
+    nav.addClass('stuck');
+  }else{
+    nav.removeClass('stuck');
+  }
+}
+
+if( $('.intro').hasClass('has-nav') ){
+  $(document).ready(function() { stickNav(); });
+  $(document).scroll(function() { stickNav(); });
+}
+
+
+
+
+///////////////////////////////////////
+//       Background overlay fade
+///////////////////////////////////////
+
+function overlay_bg(){
+	var st = $(document).scrollTop();
+	var wh = $(window).height();
+
+  $('.js-bg-fade').each(function(){
+
+    var height = $(this).height();
+    var distance = $(this).offset().top;
+    var progress = st - distance;
+
+    $(this).find('.js-bg-fade__overlay').css({
+      "opacity": progress / height
+    });
+
+  });
+
+}
+
+$(document).ready(function() {
+	overlay_bg();
+});
+
+$(document).scroll(function() {
+	overlay_bg();
+});
+
