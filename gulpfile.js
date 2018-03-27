02295ed4cca6eabb8b3612fa353eb92e25b6f48f
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-03-27 15:12:33 +0100

    rough site

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('serve', ['build-jekyll'], function() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/_templates/destination-topic': '_site/'
+        '/wales': '_site/'
       }
     }
   });
